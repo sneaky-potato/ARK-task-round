a05de02869a7458d8e97027c990705a4392e31be
Ashwani Kumar Kamal <75236490+sneaky-potato@users.noreply.github.com>
2021-06-20 15:25:08 +0530

    Add files via upload

--- Task-5/gym_tictactoe/envs/tictactoe_env.py
+++ Task-5/gym_tictactoe/envs/tictactoe_env.py
@@ -65,86 +65,84 @@ class TicTacToeEnv(gym.Env):
   _PLAYERS = [_EMPTY, _X, _O]
   _PLAYERS_COUNT = 2
   _DIM_SIZE = 3
-  _WORLDSIZE = _DIM_SIZE ** 1
-  _MAX_ROUND = 3*3
+  _WORLDSIZE = _DIM_SIZE ** 3
+  _MAX_ROUND = 3 * 3 * 3
 
   def __init__(self, **kwargs):
     self._round = 0
     self._result = None
-    self._world = [[self._get_empty(c, r) for r in range(TicTacToeEnv._DIM_SIZE)] for c in range(TicTacToeEnv._DIM_SIZE)]
+    self._world = [[[self._get_empty(b, c, r) for r in range(TicTacToeEnv._DIM_SIZE)] for c in range(TicTacToeEnv._DIM_SIZE)] for b in range(TicTacToeEnv._DIM_SIZE)]
     self._done = True
     self.enforce_rounds = True
     if kwargs:
       self.enforce_rounds = kwargs[TicTacToeEnvKeys.KW_ENFORCE_ROUNDS] if TicTacToeEnvKeys.KW_ENFORCE_ROUNDS in kwargs else False
   
-  def _get_empty(self, c, r):
+  def _get_empty(self, b, c, r):
     return 0
 
   def step(self, action):
     # Play the game only if action is not None
     if action:
-      if len(action) != 3:
+      if len(action) != 4:
         raise ValueError('Bad action: {}'.format(action))
 
-      player, c, r = int(action[0]), int(action[1]), int(action[2])
-    #   if self._done:
-    #     raise ValueError('Game has ended')
+      player, b, c, r = int(action[0]), int(action[1]), int(action[2]), int(action[3])
+      # if self._done:
+      #   raise ValueError('Game has ended')
       
       if self.enforce_rounds and player != (self._round % TicTacToeEnv._PLAYERS_COUNT) + 1:
         raise ValueError('This is not {}\'s turn'.format(player))
       
       self._round += 1
       
-      if self._world[c][r] != 0:
+      if self._world[b][c][r] != 0:
         # If cell is in use, end game with reward of -1
         self._done = True
-        self._result = 0
         info = { 'round': self._round, 'next_player': 'NONE', 'error': 'Played occupied cell' }
-        return self._world, self._result, self._done, info
+        return self._world, -1, self._done, info
 
-      self._world[c][r] = player
+      self._world[b][c][r] = player
       # self._done = (_check() is not None) or (self._round == TicTacToeEnv._MAX_ROUND)
       self._result = TicTacToeEnv._check(self._world)
-      if(self._result == 1): self._result = -10
-      elif self._result == 2: self._result = 10
-      else: self._result = None
       self._done = (True if self._result else False)
       info = { 'round': self._round, 'next_player': 'NONE' if self._done else (self._round%TicTacToeEnv._PLAYERS_COUNT)+1 }
-      return self._world, self._result, self._done, info
+      return self._world, 1 if self._done else 0, self._done, info
 
-  def get_as_char(self, c, r, winning_seq=None):
-    val = self._world[c][r]
+  def get_as_char(self, b, c, r, winning_seq=None):
+    val = self._world[b][c][r]
     ret = TicTacToeEnv._PLAYERS[val]
 
-    if winning_seq and (c,r) in winning_seq:
+    if winning_seq and (b,c,r) in winning_seq:
       return ret.upper()
     else:
       return ret
 
   def reset(self):
     self._round = 0
-    self._world = [[self._get_empty(c, r) for r in range(TicTacToeEnv._DIM_SIZE)] for c in range(TicTacToeEnv._DIM_SIZE)]
+    self._world = [[[self._get_empty(b, c, r) for r in range(TicTacToeEnv._DIM_SIZE)] for c in range(TicTacToeEnv._DIM_SIZE)] for b in range(TicTacToeEnv._DIM_SIZE)]
     self._done = False
     return self._world
 
   def render(self, mode='compact', close=False):
     # winning_seq = _check() if self._done else None
 
-    for b in range(3):
-      for c in range(3):
-          print(self.get_as_char(c, b), end=' ')
+    for r in range(3):
+      for b in range(3):
+        for c in range(3):
+            print(self.get_as_char(b, c, r), end=' ')
+        print('   ', end='')
       print()
 
   def _check_indices(index1, index2, index3, world):
-    a = world[index1[0]][index1[1]]
+    a = world[index1[0]][index1[1]][index1[2]]
     if a == 0:
       return False
 
-    b = world[index2[0]][index2[1]]
+    b = world[index2[0]][index2[1]][index2[2]]
     if a != b:
       return False
 
-    c = world[index3[0]][index3[1]]
+    c = world[index3[0]][index3[1]][index3[2]]
     if b != c:
       return False
     
@@ -152,19 +150,7 @@ class TicTacToeEnv(gym.Env):
 
   def _check(world):
     # Naiive implementation (for now)
-    for b in range(3):
-        if TicTacToeEnv._check_indices((0, b),(1, b), (2, b), world):
-            return TicTacToeEnv._check_indices((0, b),(1, b), (2, b), world)
-        if TicTacToeEnv._check_indices((b,0), (b, 1), (b, 2), world):
-            return TicTacToeEnv._check_indices((b,0), (b, 1), (b, 2), world)
-    if TicTacToeEnv._check_indices((0, 0), (1, 1), (2, 2), world):
-        return TicTacToeEnv._check_indices((0, 0), (1, 1), (2, 2), world)
-    if TicTacToeEnv._check_indices((2, 0), (1, 1), (0, 2), world):
-        return TicTacToeEnv._check_indices((2, 0), (1, 1), (0, 2), world)
-    return None
-    
 
-    """
     # For each block
     for b in range(3):
       # Check each column
@@ -250,16 +236,14 @@ class TicTacToeEnv(gym.Env):
     if TicTacToeEnv._check_indices((0, 2, 2), (1, 1, 1), (2, 0, 0), world):
         return TicTacToeEnv._check_indices((0, 2, 2), (1, 1, 1), (2, 0, 0), world)
         # return sorted([(0, 2, 2), (1, 1, 1), (2, 0, 0)])
-    """
     
+    return None
+
   def show_turn(self):
     return (self._round % TicTacToeEnv._PLAYERS_COUNT) + 1
 
   def show_result(self):
-      if self._result == 10: winner = 'o'
-      elif self._result == -10: winner = 'x'
-      else: winner = 'None'
-      msg = "Winner is '{}'!".format(winner)
+      msg = "Winner is '{}'!".format(self._result)
       print("==== Finished: {} ====".format(msg))
       return self._result
   
@@ -267,7 +251,7 @@ class TicTacToeEnv(gym.Env):
     available = []
     for i in range(3):
       for j in range(3):
-        #for k in range(3):
-        if self._world[i][j] == 0:
-          available.append("{}{}".format(i, j))
+        for k in range(3):
+          if self._world[i][j][k] == 0:
+            available.append("{}{}{}".format(i, j, k))
     return available
